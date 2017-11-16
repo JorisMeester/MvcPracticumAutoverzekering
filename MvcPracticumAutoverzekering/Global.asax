c0f9d3478e985a497422fb0dac571f4029668f2f
JorisMeester <jorismeester98@gmail.com>
2017-11-16 13:08:36 +0100

    Add project files.

---
+++ MvcPracticumAutoverzekering/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPracticumAutoverzekering.MvcApplication" Language="C#" %>
---
+++ MvcPracticumAutoverzekering/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPracticumAutoverzekering.Startup))]
+namespace MvcPracticumAutoverzekering
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
